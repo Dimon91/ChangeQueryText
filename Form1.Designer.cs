f7e398f13b85eab9d3d240f0161c60310d0b9d5c
Dmitry Nevolin <nedimon91@gmail.com>
2020-04-06 22:37:24 +0500

    Добавлена возможность удалять только комментарии из текста макета

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ChangeQueryText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBoxTemplate = new System.Windows.Forms.TextBox();
+            this.textBoxQuery = new System.Windows.Forms.TextBox();
+            this.buttonTemplateToQuery = new System.Windows.Forms.Button();
+            this.buttonQueryToTemplate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxTemplate);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxQuery);
+            this.splitContainer1.Size = new System.Drawing.Size(776, 386);
+            this.splitContainer1.SplitterDistance = 371;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // textBoxTemplate
+            // 
+            this.textBoxTemplate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTemplate.Location = new System.Drawing.Point(3, 4);
+            this.textBoxTemplate.Multiline = true;
+            this.textBoxTemplate.Name = "textBoxTemplate";
+            this.textBoxTemplate.Size = new System.Drawing.Size(365, 379);
+            this.textBoxTemplate.TabIndex = 0;
+            // 
+            // textBoxQuery
+            // 
+            this.textBoxQuery.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxQuery.Location = new System.Drawing.Point(3, 4);
+            this.textBoxQuery.Multiline = true;
+            this.textBoxQuery.Name = "textBoxQuery";
+            this.textBoxQuery.Size = new System.Drawing.Size(395, 379);
+            this.textBoxQuery.TabIndex = 0;
+            // 
+            // buttonTemplateToQuery
+            // 
+            this.buttonTemplateToQuery.Location = new System.Drawing.Point(15, 404);
+            this.buttonTemplateToQuery.Name = "buttonTemplateToQuery";
+            this.buttonTemplateToQuery.Size = new System.Drawing.Size(365, 23);
+            this.buttonTemplateToQuery.TabIndex = 2;
+            this.buttonTemplateToQuery.Text = "Текст макета в запрос -->\r\n";
+            this.buttonTemplateToQuery.UseVisualStyleBackColor = true;
+            this.buttonTemplateToQuery.Click += new System.EventHandler(this.buttonTemplateToQuery_Click);
+            // 
+            // buttonQueryToTemplate
+            // 
+            this.buttonQueryToTemplate.Location = new System.Drawing.Point(390, 404);
+            this.buttonQueryToTemplate.Name = "buttonQueryToTemplate";
+            this.buttonQueryToTemplate.Size = new System.Drawing.Size(395, 23);
+            this.buttonQueryToTemplate.TabIndex = 3;
+            this.buttonQueryToTemplate.Text = "<-- Текст запроса в макет";
+            this.buttonQueryToTemplate.UseVisualStyleBackColor = true;
+            this.buttonQueryToTemplate.Click += new System.EventHandler(this.buttonQueryToTemplate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonQueryToTemplate);
+            this.Controls.Add(this.buttonTemplateToQuery);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBoxTemplate;
+        private System.Windows.Forms.TextBox textBoxQuery;
+        private System.Windows.Forms.Button buttonTemplateToQuery;
+        private System.Windows.Forms.Button buttonQueryToTemplate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+
+namespace ChangeQueryText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            changer = new Changer();
+            InitializeComponent();
+            Bind();
+
+//            changer.PropertyChanged += UpdateField;
+
+        }
+        public Changer changer;
+   
+        private void Bind()
+        {
+            textBoxQuery.DataBindings.Add("Text", changer, "QueryText");
+            textBoxTemplate.DataBindings.Add("Text", changer, "TemplateText");
+        }
+
+        private void buttonTemplateToQuery_Click(object sender, EventArgs e)
+        {
+            changer.ChangeText(Direction.TemplateToQuery);
+        }
+
+        private void buttonQueryToTemplate_Click(object sender, EventArgs e)
+        {
+            changer.ChangeText(Direction.QueryToTemplate);
+        }
+
+        private void UpdateField(object sender, PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == "QueryText")
+            {
+                textBoxQuery.Text = changer.QueryText;
+            }
+            if (e.PropertyName == "TemplateText")
+            {
+                textBoxTemplate.Text = changer.TemplateText;
+            }
+        }
+
+      }
+
+    public enum Direction
+    {
+        TemplateToQuery,
+        QueryToTemplate
+    }
+
+    public class Changer:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public string QueryText {
+            get { return queryText; }
+            set {
+                if (queryText != value)
+                {
+                    queryText = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string TemplateText
+        {
+            get { return templateText; }
+            set {
+                if (templateText != value)
+                {
+                    templateText = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        //public event EventHandler<EventArgs> QueryTextChanged;
+        //public event EventHandler<EventArgs> TemplateTextChanged;
+
+        private string queryText;
+        private string templateText;
+
+        private string columnNameTemplate = "Template";
+        private string columnNameQuery    = "Query";
+
+        private DataTable relations = null;
+
+        public Changer()
+        {
+            relations = new DataTable();
+            var StringType = "".GetType();
+            relations.Columns.Add(columnNameTemplate, StringType);
+            relations.Columns.Add(columnNameQuery, StringType);
+
+            relations.Rows.Add("\r\n|", "\r\n");
+            relations.Rows.Add("&gt;", ">");
+            relations.Rows.Add("&lt;", "<");
+            relations.Rows.Add("&amp;", "&");
+            relations.Rows.Add("\"\"", "\"");
+
+ //           QueryTextChanged = new EventHandler<EventArgs>(EventArgs.Empty);
+        }
+
+        public void ChangeText(Direction direction)
+        {
+            string columnNameIn;
+            string columnNameOut;
+            string textIn;
+            string textOut;
+            switch (direction)
+            {
+                case Direction.TemplateToQuery:
+                    columnNameIn  = columnNameTemplate;
+                    columnNameOut = columnNameQuery;
+
+                    textIn  = templateText;
+ //                   textOut = queryText;
+                    break;
+
+                case Direction.QueryToTemplate:
+                    columnNameIn  = columnNameQuery;
+                    columnNameOut = columnNameTemplate;
+                    textIn  = queryText;
+ //                   textOut = templateText;
+                    break;
+
+                default:
+                    return;
+            }
+
+            textOut = textIn;
+            foreach(DataRow row in relations.Rows)
+            {
+                var before = row.Field<string>(columnNameIn);
+                var after  = row.Field<string>(columnNameOut);
+                textOut = textOut.Replace(before, after);
+            }
+
+            switch (direction)
+            {
+                case Direction.TemplateToQuery:
+                    QueryText = textOut;
+                    break;
+
+                case Direction.QueryToTemplate:
+                    TemplateText = textOut;
+                    break;
+
+                default:
+                    return;
+            }
+        }
+
+    }
+}
